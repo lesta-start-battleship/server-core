decd5328d6fde1c8d7416b470065f05661b9310b
Imil <ibragimovimil0@gmail.com>
2025-07-05 04:24:33 +0300

    [feat] Add Api Layer

---
+++ internal/api/websocket/router.go
@@ -0,0 +1,17 @@
+package websocket
+
+import (
+	"lesta-battleship/server-core/internal/multiplayer"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter(router gin.IRouter, websocketServer *WebsocketServer, engine *multiplayer.Engine) {
+	randomHandler := HandleGetJoinRandom(websocketServer, engine)
+	rankedHandler := HandleGetJoinRanked(websocketServer, engine)
+	customHandler := HandleGetJoinCustom(websocketServer, engine)
+
+	router.GET("/join/random", randomHandler)
+	router.GET("/join/ranked", rankedHandler)
+	router.GET("/join/custom", customHandler)
+}
