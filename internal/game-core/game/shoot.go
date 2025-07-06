2904a1679d8f0302139f1d00d1a3649f7e9047a6
Azlarkhon <xasanxojaevazlarxon2004@gmail.com>
2025-07-06 10:20:12 +0500

    add items logic

--- internal/game-core/game/shoot.go
+++ internal/game-core/game/shoot.go
@@ -2,13 +2,12 @@ package game
 
 import (
 	"errors"
-	// "log"
 )
 
 type ShootCommand struct {
-	Target Coord // получаем при создании
-	PrevState   CellState
-	Success bool
+	Target    Coord // получаем при создании
+	PrevState CellState
+	Success   bool
 }
 
 func NewShootCommand(target Coord) *ShootCommand {
@@ -28,13 +27,12 @@ func (c *ShootCommand) Apply(states *States) error {
 			ship.Decks[c.Target] = Hit
 			ship.Health -= 1
 			if ship.Health == 0 {
-     			gs.NumShips -= 1
-    		}
+				gs.NumShips -= 1
+			}
 			c.Success = true
 		}
-	} 
+	}
 	gs.Field[c.Target.X][c.Target.Y].State = Open
-	// gs.ShotsMade = append(gs.ShotsMade, c.Target)
 	return nil
 }
 
@@ -44,10 +42,9 @@ func (c *ShootCommand) Undo(states *States) {
 		ship := gs.Ships[c.PrevState.ShipID]
 		ship.Decks[c.Target] = Whole
 		if ship.Health == 0 {
-     		gs.NumShips += 1
-    	}
+			gs.NumShips += 1
+		}
 		ship.Health += 1
 	}
 	gs.Field[c.Target.X][c.Target.Y].State = c.PrevState.State
-	// gs.ShotsMade = gs.ShotsMade[:len(gs.ShotsMade)-1]
 }
