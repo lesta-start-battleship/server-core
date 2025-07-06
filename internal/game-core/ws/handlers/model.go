a56b6db7d4ca8b91ee9f98d30de0c17eade36c98
Azlarkhon <xasanxojaevazlarxon2004@gmail.com>
2025-07-06 11:02:31 +0500

    add response models

--- internal/game-core/ws/handlers/model.go
+++ internal/game-core/ws/handlers/model.go
@@ -29,16 +29,12 @@ type ShipPlacedResponse struct {
 	Coords []game.Coord `json:"coords"`
 }
 
-type ReadyConfirmedResponse struct {
-	AllReady bool `json:"all_ready"`
-}
-
-type GameStartResponse struct {
-	FirstTurn string `json:"first_turn"`
-}
-
 type ShipRemovedResponse struct {
 	Coords []game.Coord `json:"coords"`
+}
+
+type ReadyConfirmedResponse struct {
+	AllReady bool `json:"all_ready"`
 }
 
 type ShootResultResponse struct {
@@ -48,6 +44,10 @@ type ShootResultResponse struct {
 	Hit      bool   `json:"hit"`
 	NextTurn string `json:"next_turn"`
 	GameOver bool   `json:"game_over"`
+}
+
+type GameStartResponse struct {
+	FirstTurn string `json:"first_turn"`
 }
 
 type GameEndResponse struct {
